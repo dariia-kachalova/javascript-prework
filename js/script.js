25a1b3366bfb748b2338f8c7ed80ef9e38ef19f3
Dariia Kachalova <danniednepr@gmail.com>
2021-07-26 21:00:48 +0200

    js/script.js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var computerMove;
+computerMove = computerMove;
+printMessage('Zagrałem ' + '! Jeśli Twój ruch to papier, to wygrywasz!' + 'kamień');
+var playerMove;
+playerMove = 'papier';
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
