2682794faa39310822970edc818ef688c122e221
Dariia Kachalova <danniednepr@gmail.com>
2021-07-27 19:34:56 +0200

    js/script.js

--- js/script.js
+++ js/script.js
@@ -5,10 +5,23 @@ if (randomNumber == '1') {
   computerMove = 'kamień';
 } else {
   computerMove = 'nieznany ruch';
-}
- else if (randomNumber == '2') {
+}else if (randomNumber == '2') {
 	computerMove = 'papier';
+}else if (randomNumber == '3'){
+  computerMove = 'nozyce';
 }
-else if (randomNumber == '3'){
-  computerMove = "nozyce";
+
+var playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else {
+  playerMove = 'nieznany ruch';
+}
+printMessage('Twój ruch: ' + playerMove);
+} else if (playerInput =='2') {
+  playerMove = 'papier';
+} else if (playerInput == '3'){
+  playerMove = 'nozyce'
 }
