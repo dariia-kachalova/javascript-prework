b9a81c84bcfd894f0d2e8891df6d193e448c34a6
Dariia Kachalova <danniednepr@gmail.com>
2021-07-27 20:24:11 +0200

    git add js/script.js

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@ randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
 if (randomNumber == '1') {
   computerMove = 'kamień';
-} else {
-  computerMove = 'nieznany ruch';
-}else if (randomNumber == '2') {
+}
+else if (randomNumber == '2') {
 	computerMove = 'papier';
-}else if (randomNumber == '3'){
+}
+else if (randomNumber == '3'){
   computerMove = 'nozyce';
 }
 
@@ -16,12 +16,10 @@ playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 console.log('Wpisana odpowiedź to: ' + playerInput);
 if (playerInput == '1') {
   playerMove = 'kamień';
-} else {
-  playerMove = 'nieznany ruch';
 }
-printMessage('Twój ruch: ' + playerMove);
-} else if (playerInput =='2') {
+else if (playerInput =='2') {
   playerMove = 'papier';
-} else if (playerInput == '3'){
-  playerMove = 'nozyce'
+}
+else if (playerInput == '3'){
+  playerMove = 'nozyce';
 }
