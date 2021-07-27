0092c2969f3ce10f1546caa0e48b1528c9e4de05
Dariia Kachalova <danniednepr@gmail.com>
2021-07-27 18:42:26 +0200

    git add js/script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
-var computerMove;
-computerMove = computerMove;
-printMessage('Zagrałem ' + '! Jeśli Twój ruch to papier, to wygrywasz!' + 'kamień');
-var playerMove;
-playerMove = 'papier';
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else {
+  computerMove = 'nieznany ruch';
+}
+ else if (randomNumber == '2') {
+	computerMove = 'papier';
+}
+else if (randomNumber == '3'){
+  computerMove = "nozyce";
+}
